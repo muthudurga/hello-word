06497868b4c1dde7cb334e386de4b5168011468f
muthudurga <37535321+muthudurga@users.noreply.github.com>
2019-08-07 11:41:11 +0530

    Add files via upload

---
+++ vowinstr.py
@@ -0,0 +1,16 @@
+S=input()
+z=[]
+for i in range(len(S)):
+    z.append(S[i])
+if('a' in z):
+    print('yes')
+elif('e' in z):
+    print('yes')
+elif('i' in z):
+    print('yes')
+elif('o' in z):
+    print('yes')
+elif('u' in z):
+    print('yes')
+else:
+    print('no')
